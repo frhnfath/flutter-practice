d8b66559bf3bf1bfe1653ad1fdb05fdb9a0d9993
Farhan Fathurrahman <frhnfath_farhan@apps.ipb.ac.id>
2022-10-27 12:17:37 +0700

    flutter state management provider

---
+++ provider_app/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() => runApp(
+    ChangeNotifierProvider(create: (context) => Counter(), child: MyApp()));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: CounterApp(),
+    );
+  }
+}
+
+class Counter with ChangeNotifier {
+  int _number = 0;
+
+  int get number => _number;
+
+  void increment() {
+    _number++;
+    notifyListeners();
+  }
+
+  void decrement() {
+    _number--;
+    notifyListeners();
+  }
+}
+
+class CounterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+      ),
+      body: Center(
+          child: Text(
+              'You have pushed the button ${Provider.of<Counter>(context).number} many times')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () =>
+            Provider.of<Counter>(context, listen: false).increment(),
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
