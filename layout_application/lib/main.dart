ad84b65a1ad61b30a71bf63bae7da1cb0fbd3a7c
Farhan Fathurrahman <frhnfath_farhan@apps.ipb.ac.id>
2022-10-27 10:19:58 +0700

    flutter layout 4 (listview)

--- layout_application/lib/main.dart
+++ layout_application/lib/main.dart
@@ -5,48 +5,30 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  final items = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+
   @override
   Widget build(BuildContext context) {
-    @override
-    final ratingSystem = Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(Icons.star, color: Colors.green),
-        Icon(Icons.star, color: Colors.green),
-        Icon(Icons.star, color: Colors.green),
-        Icon(Icons.star),
-        Icon(Icons.star)
-      ],
-    );
-
     return MaterialApp(
       title: 'Welcome to Flutter',
       home: Scaffold(
           appBar: AppBar(
             title: Text('Welcome to Flutter'),
           ),
-          body: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Column(
-                  children: [
-                    Image.asset('images/gambar.png'),
-                    Center(child: Text("Farhan F")),
-                    ratingSystem
-                  ],
-                ),
-                Container(
-                    color: Colors.grey,
-                    padding: EdgeInsets.all(10.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text('10 Points'),
-                        Text('20 Exp'),
-                        Text('30 Contributes'),
-                      ],
-                    ))
-              ])),
+          body: ListView.separated(
+            padding: EdgeInsets.all(10.0),
+            itemCount: items.length,
+            itemBuilder: (BuildContext ctx, int index) {
+              return Container(
+                child: Text('Level ${items[index]}'),
+                height: 100,
+                decoration: BoxDecoration(
+                    border: Border(bottom: BorderSide(color: Colors.grey))),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index) =>
+                const Divider(),
+          )),
     );
   }
 }
